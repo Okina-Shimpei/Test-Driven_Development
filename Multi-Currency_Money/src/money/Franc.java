c22a5977119a28302117c195a83adf24aa749f1b
Okina-Shimpei <okinashinpei@gmail.com>
2020-01-28 21:40:35 +0900

    第5章 原則をあえて破るとき P31

---
+++ Multi-Currency_Money/src/money/Franc.java
@@ -0,0 +1,15 @@
+package money;
+
+public class Franc {
+	private int amount;
+	public Franc(int amount) {
+		this.amount = amount;
+	}
+	public Franc times(int multiplier) {
+		return new Franc(amount * multiplier);
+	}
+	public boolean equals(Object obj) {
+		Franc franc = (Franc) obj;
+		return amount == franc.amount;
+	}
+}
