79ce1ea25779eb1d13955e80ad8b851e637a8327
Okina-Shimpei <okinashinpei@gmail.com>
2020-01-29 23:22:22 +0900

    第8章 実装を隠す P50

--- Multi-Currency_Money/src/money/Money.java
+++ Multi-Currency_Money/src/money/Money.java
@@ -1,10 +1,14 @@
 package money;
 
-public class Money {
+public abstract class Money {
 	protected int amount;
+	abstract Money times(int multiplier);
 	public boolean equals(Object obj) {
 		Money money = (Money) obj;
 		return amount == money.amount
 				&& getClass().equals(money.getClass());
+	}
+	static Money dollar(int amount) {
+		return new Dollar(amount);
 	}
 }
