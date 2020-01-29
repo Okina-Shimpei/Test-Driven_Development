c36ce4c8a2dc3aa6dbc849f28cd7d0d98cf0426f
Okina-Shimpei <okinashinpei@gmail.com>
2020-01-29 23:06:34 +0900

    第6章 テスト不足に気づいたら P41

--- Multi-Currency_Money/src/money/Franc.java
+++ Multi-Currency_Money/src/money/Franc.java
@@ -1,15 +1,10 @@
 package money;
 
-public class Franc {
-	private int amount;
+public class Franc extends Money {
 	public Franc(int amount) {
 		this.amount = amount;
 	}
 	public Franc times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-	public boolean equals(Object obj) {
-		Franc franc = (Franc) obj;
-		return amount == franc.amount;
 	}
 }
